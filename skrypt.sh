db344629f1374c965992ba3db722847e5ddd4f29
Kremowkapapaja1 <radoslaw.antoni.czerwinski@gmail.com>
2023-05-22 23:34:04 +0200

    Dodanie opcji wyboru flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-if [ ["$1" == "==date"] ]; then
+if [ ["$1" == "==date" || "$1" == "-d"] ]; then
     echo "Dzisiejsza data: $(date)"
 
-if [ ["$1" == "--logs"] ]; then
+if [ ["$1" == "--logs" || "$1" == "-l" ] ]; then
     for ((i=1; i<=100; i++)); do
         filename="log${i}.txt"
         echo "Nazwa pliku: ${filename}" > "${filename}"
         echo "Nazwa skryptu: skrypt.sh" >> "${filename}"
         echo "Data: $(date)" >> "${filename}"
     done
+    echo "Utworzono 100logów"
+else
+    echo "nieznana flaga"
 
 if [[ "$1" =~ ^[0-9]+$ ]]; then
     count="$1"
@@ -23,11 +26,11 @@ for ((i=1; i<=count; i++)); do
     echo "Data: $(date)" >> "${filename}"
 done
 
-if [[ "$1" == "--help" ]]; then
+if [[ "$1" == "--help" || "$1" == "-h"]]; then
     echo "Dostępne opcje:"
-    echo "  --date: Wyświetla dzisiejszą datę."
-    echo "  --logs [liczba]: Tworzy pliki logx.txt, gdzie x to numer pliku od 1 do liczba (domyślnie 100)."
-    echo "  --help: Wyświetla dostępne opcje."
+    echo "  --date lub -d: Wyświetla dzisiejszą datę."
+    echo "  --logs [liczba] lub -l [liczba]: Tworzy pliki logx.txt, gdzie x to numer pliku od 1 do liczba (domyślnie 100)."
+    echo "  --help lub -h: Wyświetla dostępne opcje."
 else
-    echo "Nieznana opcja. Użyj --help, aby zobaczyć dostępne opcje."
+    echo "Nieznana opcja. Użyj --help lub -h, aby zobaczyć dostępne opcje."
 fi
