ebb6ec1b67941ea36ce61b6188d0264ad91f142f
Kremowkapapaja1 <radoslaw.antoni.czerwinski@gmail.com>
2023-05-22 22:55:03 +0200

    dodanie help

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,16 @@ if [ ["$1" == "--logs"] ]; then
         echo "Nazwa skryptu: skrypt.sh" >> "${filename}"
         echo "Data: $(date)" >> "${filename}"
     done
+
+if [[ "$1" =~ ^[0-9]+$ ]]; then
+    count="$1"
+else
+    count=100
+fi
+
+for ((i=1; i<=count; i++)); do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: ${filename}" > "${filename}"
+    echo "Nazwa skryptu: skrypt.sh" >> "${filename}"
+    echo "Data: $(date)" >> "${filename}"
+done
