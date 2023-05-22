cdf0b1b7de9c56f86ff6a68ffeebe51fdfa6866b
Kremowkapapaja1 <radoslaw.antoni.czerwinski@gmail.com>
2023-05-22 22:45:37 +0200

    Naprawa logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Dzisiejsza data: $(date)"
+if [ ["$1" == "==date"] ]; then
+    echo "Dzisiejsza data: $(date)"
+
+if [ ["$1" == "--logs"] ]; then
+    for ((i=1; i<=100; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: ${filename}" > "${filename}"
+        echo "Nazwa skryptu: skrypt.sh" >> "${filename}"
+        echo "Data: $(date)" >> "${filename}"
+    done
